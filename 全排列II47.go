fbe1df0e88362bf782ac13343adc415d55acf21e
Cpxin <1099402716@qq.com>
2019-01-30 21:39:29 +0800

    20190130

--- 全排列II47.go
+++ 全排列II47.go
@@ -1,9 +1,56 @@
 package main
 
-func permuteUnique(nums []int) [][]int {
+import (
+	"fmt"
+	"time"
+)
 
+var Nums =[][]int{}
+var Munms=make(map[string]int)
+
+func permuteUnique(nums []int) [][]int {
+	t1 := time.Now()
+	end:=[]int{}
+	Nums=nil
+	Munms=make(map[string]int)
+	if len(nums)==0||len(nums)==1 {
+		Nums=append(Nums,nums)
+		return Nums
+	}
+	first(nums,end)
+	elapsed := time.Since(t1)
+	fmt.Println(elapsed)
+	return Nums
+
+}
+func first(nums []int,end []int)  {
+	if len(nums)!=0 {
+		Tnums:=nums
+		for i:=0;i<len(Tnums) ;i++  {
+			Tend:=append(end[:len(end):len(end)],Tnums[i])
+			nums=append(Tnums[:i:i],Tnums[i+1:]...)
+			first(nums,Tend)
+			Tend=Tend[:len(Tend)-1]
+		}
+	}else {
+		temp:=""
+		for _,v:=range end{
+			temp+=fmt.Sprintf("%d",v)
+		}
+		Munms[temp]++
+		if Munms[temp]==1 {
+			Nums=append(Nums,end)
+		}
+	}
 }
 
 func main() {
-	
+
+	nums:=[]int{1,2,3,4}
+
+	fmt.Println(permuteUnique(nums))
+
+
+
+
 }
